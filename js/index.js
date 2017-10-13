1b2563c3b44c75c6da38b21f84506cbb21f793e8
AlirazaFazal <31009341+AlirazaFazal@users.noreply.github.com>
2017-10-13 11:47:32 +0100

    Web socket integration and updated code for gauges
    Gauges code updated;
    -NL
    -AJ
    -Mach No
    -Rocket

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+var gauges = [];
+var lgauges = [];
+var bgauges = [];
 var tbtvalue = Number($('.tbt-percentage').data()); //tbt percentage websocket hookup
 
 var nhvalue = Number($('.nh-percentage').data()); //nh percentage websocket hookup
@@ -33,7 +36,7 @@ var $linearGauge3 = $("#distancegauge");
                 ],
                 minimumValue: 0,
                 maximumValue: 1000,
-                value: 350, //The value that changes the pointer for the distance gauge
+                value: 0, //The value that changes the pointer for the distance gauge
                 tickBrush: '#F79646',
                 minorTickBrush: '#F79646',
                 needleShape:'triangle',
@@ -76,7 +79,7 @@ var $linearGauge1 = $("#voltagegauge");
                 ], 
                 minimumValue: 0,
                 maximumValue: 100,
-                value: 50, //value that changes that pointer for the voltage
+                value: 0, //value that changes that pointer for the voltage
                 tickBrush: '#F79646',
                 minorTickBrush: '#F79646',
                 needleShape:'triangle',
@@ -88,11 +91,15 @@ var $linearGauge1 = $("#voltagegauge");
                 }
             });
 
+lgauges["distance"] = $linearGauge3;
+lgauges["volt"] = $linearGauge1;
+			
 $(function () {
             /*----------------- Instantiation -------------------------*/
-            var $lineargauge = $("#fronttempgauge");
+            var $lineargaugefront = $("#fronttempgauge");
+lgauges["ftemp"] = $lineargaugefront;
 
-            $lineargauge.igLinearGauge({
+            $lineargaugefront.igLinearGauge({
                 width: '22%',
                 height: '35vw',
                 orientation: "vertical",
@@ -144,11 +151,13 @@ $(function () {
 
         });
 
+
 $(function () {
             /*----------------- Instantiation -------------------------*/
-            var $lineargauge = $("#reartempgauge");
+            var $lineargaugerear = $("#reartempgauge");
+			lgauges["rtemp"] = $lineargaugerear;
 
-            $lineargauge.igLinearGauge({
+            $lineargaugerear.igLinearGauge({
                 width: '22%',
                 height: '35vw',
                 orientation: "vertical",
@@ -288,8 +297,10 @@ Highcharts.chart('speedometer', {
     // This code pretty much randomises the pointer for the gauge
     function (chart) {
         if (!chart.renderer.forExport) {
-            setInterval(function () {
-                var point = chart.series[0].points[0],
+		    gauges["speedometer"] = chart.series[0].points[0];
+            /*setInterval(function () {
+              gauges["speedometer"] = chart.series[0].points[0];
+              var point = chart.series[0].points[0],
                     newVal,
                     inc = Math.round((Math.random() - 0.5) * 20);
 
@@ -300,7 +311,7 @@ Highcharts.chart('speedometer', {
 
                 point.update(newVal);
 
-            }, 3000);
+            }, 3000);*/
         }
     });
 
@@ -393,7 +404,8 @@ Highcharts.chart('rpm', {
     // Add some life
     function (chart) {
         if (!chart.renderer.forExport) {
-            setInterval(function () {
+            gauges["rpm"] = chart.series[0].points[0];
+			/*setInterval(function () {
                 var point = chart.series[0].points[0],
                     newVal,
                     inc = Math.round((Math.random() - 0.5) * 20);
@@ -405,9 +417,10 @@ Highcharts.chart('rpm', {
 
                 point.update(newVal);
 
-            }, 3000);
+            }, 3000);*/
         }
-    });
+    }
+	);
 
 Highcharts.chart('fuel', {
 
@@ -499,7 +512,8 @@ Highcharts.chart('fuel', {
     // Add some life
     function (chart) {
         if (!chart.renderer.forExport) {
-            setInterval(function () {
+            gauges["fuel"] = chart.series[0].points[0];
+            /*setInterval(function () {
                 var point = chart.series[0].points[0],
                     newVal,
                     inc = Math.round((Math.random() - 0.5) * 20);
@@ -511,7 +525,7 @@ Highcharts.chart('fuel', {
 
                 point.update(newVal);
 
-            }, 3000);
+            }, 3000);*/
         }
     });
 
@@ -537,7 +551,7 @@ var circle = new ProgressBar.Circle(rocket, {
 });
 
 // This will get the number from the page
-    var value = (90 / 100);
+    var value = (10 / 100);
     
 
 // This will determine the circumference of the circle
@@ -555,7 +569,7 @@ circle.animate(value, {
 
   }
 });
-
+bgauges["rocket"] = bar;
 
 var startColor = '#ff33cc';
 var endColor = '#ff33cc';
@@ -578,7 +592,7 @@ var circle = new ProgressBar.Circle(machno, {
 });
 
 // This will get the number from the page
-    var value = (90 / 100);
+    var value = (50 / 100);
     
 
 // This will determine the circumference of the circle
@@ -595,6 +609,7 @@ circle.animate(value, {
     }
         }
 });
+bgauges["mach"] = bar;
 
 var bar = new ProgressBar.Circle(nl, { //this gauge is for NL within the speedometer which is visible on dashboard 
   color: '#00ccff',
@@ -628,6 +643,7 @@ bar.text.style.fontSize = '1vw';
 
 var stopvalue = 50; // websocket integration
 bar.animate(0.75 * stopvalue/100);  // scale for bar on gauge
+bgauges["nl"] = bar;
 
 var bar = new ProgressBar.SemiCircle(aj, { //this gauge is for AJ within the speedometer which is visible on dashboard 
   strokeWidth: 7,
@@ -645,8 +661,7 @@ var bar = new ProgressBar.SemiCircle(aj, { //this gauge is for AJ within the spe
   // Set default step function for all animate calls
   step: (state, bar) => {
     bar.path.setAttribute('stroke', state.color);
-    var value = Math.round(bar.value() * 200); 
-      console.log(bar.value());
+    var value = Math.round(bar.value() * 200); //here value for gauge is being set
     if (value === 0) {
       bar.setText('AJ');
     } else {
@@ -658,7 +673,69 @@ var bar = new ProgressBar.SemiCircle(aj, { //this gauge is for AJ within the spe
 });
 bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
 bar.text.style.fontSize = '1vw';
+
 var stopvalue = 50; //here value for gauge is being set
 bar.animate(0.5 * stopvalue/100);  // scale for bar on gauge 
+bgauges["aj"] = bar;
+
+
 
 // segDisplay.value(56749);
+
+function updateGauges(msg)
+{
+	msg = JSON.parse(msg);
+	gauges["speedometer"].update(msg.RS232_P2_FUEL_GPS_mph);
+	gauges["rpm"].update(msg.A1_12_BR_RPM); //msg.A1_28_BL_RPM
+	gauges["fuel"].update(msg.CALC_JetFuelRemainingPerc);
+	lgauges["distance"].igLinearGauge({value: msg.CLAC_Distance_Travelled});
+	lgauges["volt"].igLinearGauge({value: msg.A2_26_POWER_BUS_VOLTAGE_SIG});
+	lgauges["ftemp"].igLinearGauge({value: msg.A2_2_FR_TC2_OUTER_BRK_PAD_TEMP});
+	lgauges["rtemp"].igLinearGauge({value: msg.A2_2_BR_TC2_OUTER_BRK_PAD_TEMP}); 
+	
+	bgauges["mach"].animate(msg.CALC_MachNo);
+	//bgauges["rocket"].animate(0.1);
+	bgauges["nl"].animate(msg.MILBUS_DECUJetNL);
+	bgauges["aj"].animate(msg.MILBUS_DECUJetNozArea);
+
+	$('.tbt-percentage').text(msg.CALC_DECUJetTBTPercent+" %TBT"); 
+	$('.nh-percentage').text(msg.MILBUS_DECUJetNH+" %NH"); 
+	
+}
+
+document.body.onload = function() {
+
+	create_socket();
+}
+var prev_ts = -1;
+var socket = null;
+function close_socket()
+{
+	if (socket)
+		socket.close();
+	socket = null;
+}
+function create_socket()
+{
+	// First the socket requesta
+	console.log('Creating socket');
+	socket = new WebSocket('ws://129.146.69.162:8282/');
+
+	socket.onopen = function() {
+
+	  console.log('Socket open.');
+	  socket.send(JSON.stringify({message: 'Client connected '}));
+	  console.log('Message sent.')
+	};
+	socket.onmessage = function(message) {
+	  if (message && message.data)
+	  {
+		  msg = JSON.parse(message.data);
+		  if (prev_ts == -1)
+			prev_ts = msg.currentEPOC;
+		  
+		  var offset = msg.currentEPOC - prev_ts;
+		  window.setTimeout("updateGauges('"+message.data+"')", offset);
+	  }
+	};
+}
